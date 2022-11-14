1222e17cab3889bf45cfc2384c14f648dc63b76a
dnisc <dnisc@DNISCHE-MORSKOE>
2022-11-14 13:43:57 +0300

    Добавление Model

---
+++ WPFAssemblyBrowser/Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using MPP3;
+
+namespace WPFAssemblyBrowser
+{
+    public class Model  : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string assemblyFileName;
+        private ObservableCollection<AssemblyInfo> assemblyInfo;
+        public string AssemblyFileName
+        {
+            get => assemblyFileName;
+            set
+            {
+                assemblyFileName = value;
+                OnPropertyChanged("AssemblyFileName");
+            }
+        }
+
+        public ObservableCollection<AssemblyInfo> AssemblyData
+        {
+            get => assemblyInfo;
+            set
+            {
+                assemblyInfo = value;
+                OnPropertyChanged("AssemblyInfo");
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
